2670fb1e419423558f6e954de806a5e2dd41de03
rover <110656154+Irhis13@users.noreply.github.com>
2025-10-06 18:58:49 +0200

    Commit inicial

---
+++ src/main/java/com/rv/banco/GestionCuentasBancariasApplication.java
@@ -0,0 +1,13 @@
+package com.rv.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCuentasBancariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCuentasBancariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rv/banco/GestionCuentasBancariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rv.banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCuentasBancariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
